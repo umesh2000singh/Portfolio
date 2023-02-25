74b0e2d1fcdcefa46081a8d5f00665e732fea957
umesh2000singh <umeshsinghmehta72@gmail.com>
2023-02-26 00:46:01 +0530

    landing page done

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ function menu_reveal() {
 };
 
 //main body
-document.querySelector(".fa-angle-right").addEventListener("click", test)
-function test(){
-    document.querySelector(".content2").classList.toggle("content2_clicked");
-    document.querySelector(".content1").classList.toggle("content1_clicked");
+// corousal logic
+document.querySelector(".fa-angle-right").addEventListener("click", next)
+function next(){
+    document.querySelector(".content1").classList.add("content1_clicked");
+    document.querySelector(".content2").classList.add("content2_clicked");
+    document.querySelector(".photo1").classList.add("photo1_clicked");
+    document.querySelector(".photo2").classList.add("photo2_clicked");
+    
+}
+document.querySelector(".fa-angle-left").addEventListener("click", prev);
+function prev(){
+    document.querySelector(".content1").classList.remove("content1_clicked");
+    document.querySelector(".content2").classList.remove("content2_clicked");
+    document.querySelector(".photo1").classList.remove("photo1_clicked");
+    document.querySelector(".photo2").classList.remove("photo2_clicked");
 }
