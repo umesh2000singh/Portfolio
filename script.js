af4ac3d095bd1869fbbaf6815d127f1eb6569d09
umesh2000singh <umeshsinghmehta72@gmail.com>
2023-02-28 01:21:24 +0530

    changes

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-// hamburger menu
-document.querySelector(".menu").addEventListener("click", menu_reveal);
-function menu_reveal() {
-    document.querySelector(".menu_items").classList.toggle("reveal");
-    document.querySelector(".cross").classList.toggle("cross_after");
-    document.querySelector(".menu").classList.toggle("menu_after");
-};
-
-//main body
-// corousal logic
-document.querySelector(".fa-angle-right").addEventListener("click", next)
-function next(){
-    document.querySelector(".content1").classList.add("content1_clicked");
-    document.querySelector(".content2").classList.add("content2_clicked");
-    document.querySelector(".photo1").classList.add("photo1_clicked");
-    document.querySelector(".photo2").classList.add("photo2_clicked");
-    
-}
-document.querySelector(".fa-angle-left").addEventListener("click", prev);
-function prev(){
-    document.querySelector(".content1").classList.remove("content1_clicked");
-    document.querySelector(".content2").classList.remove("content2_clicked");
-    document.querySelector(".photo1").classList.remove("photo1_clicked");
-    document.querySelector(".photo2").classList.remove("photo2_clicked");
-}
+let hamburger_button = document.querySelector(".hamburger");
+let menu = document.querySelector(".menu");
+hamburger_button.addEventListener("click", ()=>{
+    menu.classList.toggle("menu_show");
+    hamburger_button.classList.toggle("hamburger_clicked")
+});
