e24364ebd64aab48bec1abeed5d544cbf94181cb
umesh2000singh <umeshsinghmehta72@gmail.com>
2023-03-01 22:04:57 +0530

    scroll animation

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ hamburger_button.addEventListener("click", ()=>{
     menu.classList.toggle("menu_show");
     hamburger_button.classList.toggle("hamburger_clicked")
 });
+// scroll animation
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 100
+});
 
+ScrollReveal().reveal('.right', { origin: 'left' });
+ScrollReveal().reveal('.left', { origin: 'bottom' });
 
 // typejs
 const typed = new Typed('.multi-text', {
     strings: ['Architect.', 'Chomu.', 'Suar'],
     typeSpeed:100,
-    backSpeed:100,
+    backSpeed:10,
     backDelay:1000,
     loop: true
 })
