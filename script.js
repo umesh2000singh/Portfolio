2e5d09b57ad15a01ccc72e2bfa40ee3323547786
umesh2000singh <umeshsinghmehta72@gmail.com>
2023-03-01 21:34:11 +0530

    change

--- script.js
+++ script.js
@@ -4,16 +4,7 @@ hamburger_button.addEventListener("click", ()=>{
     menu.classList.toggle("menu_show");
     hamburger_button.classList.toggle("hamburger_clicked")
 });
-// scroll animation
-ScrollReveal({
-    reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 100
-});
 
-ScrollReveal().reveal('.right', { origin: 'left' });
-ScrollReveal().reveal('.left', { origin: 'bottom' });
 
 // typejs
 const typed = new Typed('.multi-text', {
