b505148b8ec2ccbca1ea767596d2be5fcba97d84
umesh2000singh <umeshsinghmehta72@gmail.com>
2023-03-08 01:20:22 +0530

    project secction done

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ const typed = new Typed('.multi-text', {
     loop: true
 })
 
+// for projects
+let right = document.querySelector(".fa-angle-double-right");
+let left =  document.querySelector(".fa-angle-double-left");
+right.addEventListener("click", ()=>{
+    document.querySelector(".number").innerHTML = "2"
+    document.querySelector(".building1").classList.add("building1-clicked")
+    document.querySelector(".building2").classList.add("building2-clicked")
+    document.querySelector(".project1").classList.add("project1-clicked")
+    document.querySelector(".project2").classList.add("project2-clicked")
+})
+left.addEventListener("click",()=>{
+    document.querySelector(".number").innerHTML = "1"
+    document.querySelector(".building1").classList.remove("building1-clicked")
+    document.querySelector(".building2").classList.remove("building2-clicked")
+    document.querySelector(".project1").classList.remove("project1-clicked")
+    document.querySelector(".project2").classList.remove("project2-clicked")
+})
